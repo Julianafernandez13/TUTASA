50886e510faa66ede8f4d1aa3658f6be97cdd2ed
tinooratti <tinooratti13@gmail.com>
2026-06-01 23:06:24 -0300

    Costos vs Ventas Terminada

--- TUTASA/CostovsVentas/CostovsVentas.Designer.cs
+++ TUTASA/CostovsVentas/CostovsVentas.Designer.cs
@@ -30,21 +30,21 @@
         {
             this.lblTitulo = new System.Windows.Forms.Label();
             this.gbFiltros = new System.Windows.Forms.GroupBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.cmbAño = new System.Windows.Forms.ComboBox();
+            this.cmbMes = new System.Windows.Forms.ComboBox();
+            this.labelAño = new System.Windows.Forms.Label();
             this.cmbEmpresa = new System.Windows.Forms.ComboBox();
-            this.lblEmpresa = new System.Windows.Forms.Label();
-            this.lblDesde = new System.Windows.Forms.Label();
+            this.labelEmpresa = new System.Windows.Forms.Label();
+            this.lblMes = new System.Windows.Forms.Label();
             this.btnGenerar = new System.Windows.Forms.Button();
             this.btnLimpiar = new System.Windows.Forms.Button();
             this.lblTotales = new System.Windows.Forms.Label();
-            this.lvBultos = new System.Windows.Forms.ListView();
-            this.Empresa = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listViewResultados = new System.Windows.Forms.ListView();
             this.Clasificación = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnSalir = new System.Windows.Forms.Button();
             this.gbFiltros.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,25 +57,27 @@
             this.lblTitulo.Size = new System.Drawing.Size(528, 29);
             this.lblTitulo.TabIndex = 0;
             this.lblTitulo.Text = "Costo vs Ventas por Empresa de Transporte";
+            this.lblTitulo.Click += new System.EventHandler(this.lblTitulo_Click);
             // 
             // gbFiltros
             // 
-            this.gbFiltros.Controls.Add(this.comboBox2);
-            this.gbFiltros.Controls.Add(this.comboBox1);
-            this.gbFiltros.Controls.Add(this.label1);
+            this.gbFiltros.Controls.Add(this.cmbAño);
+            this.gbFiltros.Controls.Add(this.cmbMes);
+            this.gbFiltros.Controls.Add(this.labelAño);
             this.gbFiltros.Controls.Add(this.cmbEmpresa);
-            this.gbFiltros.Controls.Add(this.lblEmpresa);
-            this.gbFiltros.Controls.Add(this.lblDesde);
+            this.gbFiltros.Controls.Add(this.labelEmpresa);
+            this.gbFiltros.Controls.Add(this.lblMes);
             this.gbFiltros.Location = new System.Drawing.Point(60, 107);
             this.gbFiltros.Name = "gbFiltros";
             this.gbFiltros.Size = new System.Drawing.Size(660, 120);
             this.gbFiltros.TabIndex = 1;
             this.gbFiltros.TabStop = false;
             this.gbFiltros.Text = "Filtros de Consulta";
+            this.gbFiltros.Enter += new System.EventHandler(this.gbFiltros_Enter);
             // 
-            // comboBox2
+            // cmbAño
             // 
-            this.comboBox2.Items.AddRange(new object[] {
+            this.cmbAño.Items.AddRange(new object[] {
             "2016",
             "2017",
             "2018",
@@ -87,14 +89,15 @@
             "2024",
             "2025",
             "2026"});
-            this.comboBox2.Location = new System.Drawing.Point(279, 27);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(96, 28);
-            this.comboBox2.TabIndex = 8;
+            this.cmbAño.Location = new System.Drawing.Point(279, 27);
+            this.cmbAño.Name = "cmbAño";
+            this.cmbAño.Size = new System.Drawing.Size(96, 28);
+            this.cmbAño.TabIndex = 8;
+            this.cmbAño.SelectedIndexChanged += new System.EventHandler(this.cmbAño_SelectedIndexChanged);
             // 
-            // comboBox1
+            // cmbMes
             // 
-            this.comboBox1.Items.AddRange(new object[] {
+            this.cmbMes.Items.AddRange(new object[] {
             "01",
             "02",
             "03",
@@ -107,19 +110,20 @@
             "10",
             "11",
             "12"});
-            this.comboBox1.Location = new System.Drawing.Point(126, 27);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(67, 28);
-            this.comboBox1.TabIndex = 7;
+            this.cmbMes.Location = new System.Drawing.Point(126, 27);
+            this.cmbMes.Name = "cmbMes";
+            this.cmbMes.Size = new System.Drawing.Size(67, 28);
+            this.cmbMes.TabIndex = 7;
+            this.cmbMes.SelectedIndexChanged += new System.EventHandler(this.cmbMes_SelectedIndexChanged);
             // 
-            // label1
+            // labelAño
             // 
-            this.label1.Location = new System.Drawing.Point(230, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 23);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Año:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.labelAño.Location = new System.Drawing.Point(230, 30);
+            this.labelAño.Name = "labelAño";
+            this.labelAño.Size = new System.Drawing.Size(43, 23);
+            this.labelAño.TabIndex = 6;
+            this.labelAño.Text = "Año:";
+            this.labelAño.Click += new System.EventHandler(this.labelAño_Click);
             // 
             // cmbEmpresa
             // 
@@ -132,22 +136,25 @@
             this.cmbEmpresa.Name = "cmbEmpresa";
             this.cmbEmpresa.Size = new System.Drawing.Size(200, 28);
             this.cmbEmpresa.TabIndex = 0;
+            this.cmbEmpresa.SelectedIndexChanged += new System.EventHandler(this.cmbEmpresa_SelectedIndexChanged);
             // 
-            // lblEmpresa
+            // labelEmpresa
             // 
-            this.lblEmpresa.Location = new System.Drawing.Point(36, 70);
-            this.lblEmpresa.Name = "lblEmpresa";
-            this.lblEmpresa.Size = new System.Drawing.Size(84, 23);
-            this.lblEmpresa.TabIndex = 1;
-            this.lblEmpresa.Text = "Empresa:";
+            this.labelEmpresa.Location = new System.Drawing.Point(36, 70);
+            this.labelEmpresa.Name = "labelEmpresa";
+            this.labelEmpresa.Size = new System.Drawing.Size(84, 23);
+            this.labelEmpresa.TabIndex = 1;
+            this.labelEmpresa.Text = "Empresa:";
+            this.labelEmpresa.Click += new System.EventHandler(this.labelEmpresa_Click);
             // 
-            // lblDesde
+            // lblMes
             // 
-            this.lblDesde.Location = new System.Drawing.Point(68, 32);
-            this.lblDesde.Name = "lblDesde";
-            this.lblDesde.Size = new System.Drawing.Size(52, 23);
-            this.lblDesde.TabIndex = 5;
-            this.lblDesde.Text = "Mes:";
+            this.lblMes.Location = new System.Drawing.Point(68, 32);
+            this.lblMes.Name = "lblMes";
+            this.lblMes.Size = new System.Drawing.Size(52, 23);
+            this.lblMes.TabIndex = 5;
+            this.lblMes.Text = "Mes:";
+            this.lblMes.Click += new System.EventHandler(this.lblMes_Click);
             // 
             // btnGenerar
             // 
@@ -156,6 +163,7 @@
             this.btnGenerar.Size = new System.Drawing.Size(140, 35);
             this.btnGenerar.TabIndex = 4;
             this.btnGenerar.Text = "Generar";
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
             // 
             // btnLimpiar
             // 
@@ -164,6 +172,7 @@
             this.btnLimpiar.Size = new System.Drawing.Size(100, 35);
             this.btnLimpiar.TabIndex = 2;
             this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
             // 
             // lblTotales
             // 
@@ -174,31 +183,26 @@
             this.lblTotales.Size = new System.Drawing.Size(359, 25);
             this.lblTotales.TabIndex = 0;
             this.lblTotales.Text = "Totales: Ingresos / Costos / Margen";
+            this.lblTotales.Click += new System.EventHandler(this.lblTotales_Click);
             // 
-            // lvBultos
+            // listViewResultados
             // 
-            this.lvBultos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.Empresa,
+            this.listViewResultados.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.Clasificación,
             this.columnHeader1,
             this.columnHeader2,
             this.columnHeader3});
-            this.lvBultos.FullRowSelect = true;
-            this.lvBultos.GridLines = true;
-            this.lvBultos.HideSelection = false;
-            this.lvBultos.Location = new System.Drawing.Point(60, 339);
-            this.lvBultos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.lvBultos.Name = "lvBultos";
-            this.lvBultos.Size = new System.Drawing.Size(660, 158);
-            this.lvBultos.TabIndex = 7;
-            this.lvBultos.UseCompatibleStateImageBehavior = false;
-            this.lvBultos.View = System.Windows.Forms.View.Details;
-            this.lvBultos.SelectedIndexChanged += new System.EventHandler(this.lvBultos_SelectedIndexChanged);
-            // 
-            // Empresa
-            // 
-            this.Empresa.Text = "Empresa";
-            this.Empresa.Width = 90;
+            this.listViewResultados.FullRowSelect = true;
+            this.listViewResultados.GridLines = true;
+            this.listViewResultados.HideSelection = false;
+            this.listViewResultados.Location = new System.Drawing.Point(60, 339);
+            this.listViewResultados.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listViewResultados.Name = "listViewResultados";
+            this.listViewResultados.Size = new System.Drawing.Size(660, 158);
+            this.listViewResultados.TabIndex = 7;
+            this.listViewResultados.UseCompatibleStateImageBehavior = false;
+            this.listViewResultados.View = System.Windows.Forms.View.Details;
+            this.listViewResultados.SelectedIndexChanged += new System.EventHandler(this.listViewResultados_SelectedIndexChanged);
             // 
             // Clasificación
             // 
@@ -220,18 +224,31 @@
             this.columnHeader3.Text = "Margen Neto";
             this.columnHeader3.Width = 120;
             // 
-            // frmCostovsVentas
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(622, 572);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(98, 40);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // CostovsVentas
             // 
             this.ClientSize = new System.Drawing.Size(800, 626);
-            this.Controls.Add(this.lvBultos);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.listViewResultados);
             this.Controls.Add(this.lblTotales);
             this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.btnGenerar);
             this.Controls.Add(this.gbFiltros);
             this.Controls.Add(this.lblTitulo);
-            this.Name = "frmCostovsVentas";
+            this.Name = "CostovsVentas";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Costo vs Ventas";
+            this.Load += new System.EventHandler(this.CostovsVentas_Load);
             this.gbFiltros.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -241,20 +258,20 @@
         #endregion
         private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.GroupBox gbFiltros;
-        private System.Windows.Forms.Label lblDesde;
-        private System.Windows.Forms.Label lblEmpresa;
+        private System.Windows.Forms.Label lblMes;
+        private System.Windows.Forms.Label labelEmpresa;
         private System.Windows.Forms.ComboBox cmbEmpresa;
         private System.Windows.Forms.Button btnGenerar;
         private System.Windows.Forms.Button btnLimpiar;
         private System.Windows.Forms.Label lblTotales;
-        private System.Windows.Forms.ListView lvBultos;
-        private System.Windows.Forms.ColumnHeader Empresa;
+        private System.Windows.Forms.ListView listViewResultados;
         private System.Windows.Forms.ColumnHeader Clasificación;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelAño;
+        private System.Windows.Forms.ComboBox cmbAño;
+        private System.Windows.Forms.ComboBox cmbMes;
+        private System.Windows.Forms.Button btnSalir;
     }
 }
--- TUTASA/CostovsVentas/CostovsVentas.cs
+++ TUTASA/CostovsVentas/CostovsVentas.cs
@@ -7,22 +7,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TUTASA.CostovsVentas;
 
 namespace TUTASA.Forms.Administracion
 {
     public partial class CostovsVentas : Form
     {
+        private CostovsVentasModelo modelo = new CostovsVentasModelo();
+
         public CostovsVentas()
         {
             InitializeComponent();
         }
 
-        private void lvBultos_SelectedIndexChanged(object sender, EventArgs e)
+        private void CostovsVentas_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void listViewResultados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelAño_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTitulo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gbFiltros_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblMes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbMes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbAño_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelEmpresa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbEmpresa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTotales_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
         {
 
         }
