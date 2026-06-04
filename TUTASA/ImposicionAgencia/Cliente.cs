6a6f38d90440560ac5cea8d7e5c0a58935eeba31
tinooratti <tinooratti13@gmail.com>
2026-06-03 21:49:06 -0300

    Cambios en imposicion en agencia

---
+++ TUTASA/ImposicionAgencia/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUTASA.ImposicionAgencia
+{
+    internal class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public long CUIT { get; set; }
+
+    }
+}
