b62ba525f6e489ec945dbb8df0f608e914ba06ac
MaxLateano <maxlateano@gmail.com>
2026-06-04 22:16:04 -0300

    Empezando a hacer la pantalla de Registrar Entrega.

---
+++ TUTASA/RegistrarEntrega/Receptor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TUTASA.Admision;
+
+namespace TUTASA.RegistrarEntrega
+{
+    internal class Receptor
+    {
+        public int Id { get; set; }
+        public string Dni { get; set; }
+        public string NombreCompleto { get; set; }
+        public List<Guia> Guias { get; set; } = new List<Guia>();
+    }
+}
