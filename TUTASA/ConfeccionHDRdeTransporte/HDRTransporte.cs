b2d30eccb8fae032f92f12b1b163e01f056f906a
leoomusalem <musalemleo@gmail.com>
2026-06-06 15:40:54 -0300

    clase HDR de Transporte

---
+++ TUTASA/ConfeccionHDRdeTransporte/HDRTransporte.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUTASA.ConfeccionHDRdeTransporte
+{
+    internal class HDRTransporte
+    {
+        public int Id { get; set; }
+        public DateTime FechaEmision { get; set; }
+        public string Estado { get; set; }
+        public int IdEmpresaTransporte { get; set; }
+        public string TipoArrendamiento { get; set; }
+        public List<Guia> Guias { get; set; }
+    }
+}
