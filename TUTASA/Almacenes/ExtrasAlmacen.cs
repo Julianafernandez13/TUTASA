45364605d02890f387640359e6d416254c9141a5
tinooratti <tinooratti13@gmail.com>
2026-06-12 00:35:15 -0300

    avance hasta ExtrasEntidad

---
+++ TUTASA/Almacenes/ExtrasAlmacen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUTASA.Almacenes
+{
+    internal static class ExtrasAlmacen
+    {
+        private static List<ExtrasEntidad> extrass = new List<ExtrasEntidad>();
+
+        static ExtrasAlmacen()
+        {
+            if (File.Exists("extras.json"))
+            {
+                string json = File.ReadAllText("extras.json");
+                extrass = JsonSerializer.Deserialize<List<ExtrasEntidad>>(json);
+            }
+        }
+
+        public static List<ExtrasEntidad> ObtenerTodos()
+        {
+            return extrass;
+        }
+
+        public static void Guardar()
+        {
+            string json = JsonSerializer.Serialize(extrass);
+            File.WriteAllText("extras.json", json);
+        }
+    }
+}
