1af3f23f7b29a17c19814463ec646cd1fbda12f6
tinooratti <tinooratti13@gmail.com>
2026-06-13 16:43:34 -0300

    agregue using que faltaban

--- TUTASA/Almacenes/ClienteEntidad.cs
+++ TUTASA/Almacenes/ClienteEntidad.cs
@@ -6,17 +6,14 @@ using System.Threading.Tasks;
 
 namespace TUTASA.Almacenes
 {
-    internal class ClienteEntidad
+    public class ClienteEntidad
     {
-        public class ClienteEntidad
-        {
-            public int IdCliente { get; set; }
-            public long CuitCliente { get; set; }
-            public string NombreCliente { get; set; }
-            public string ApellidoCliente { get; set; }
-            public string DomicilioCliente { get; set; }
-            public long TelefonoCliente { get; set; }
-            public TipoFacturaEnum TipoFactura { get; set; }
-        }
+        public int IdCliente { get; set; }
+        public long CuitCliente { get; set; }
+        public string NombreCliente { get; set; }
+        public string ApellidoCliente { get; set; }
+        public string DomicilioCliente { get; set; }
+        public long TelefonoCliente { get; set; }
+        public TipoFacturaEnum TipoFactura { get; set; }
     }
 }
--- TUTASA/Almacenes/CodigoPostalAlmacen.cs
+++ TUTASA/Almacenes/CodigoPostalAlmacen.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace TUTASA.Almacenes
